f36cae0af43740746053e1790d5e67448e9b4ea8
Maksym Kintor <kintormaksim@gmail.com>
2023-04-17 17:53:43 +0200

    divide into login, register and index page, some fixes and styles

--- index.php
+++ index.php
@@ -16,43 +16,15 @@
 	<title>Home</title>
 	
 </head>
-<body>
-	<!-- NAVBAR -->
-	<!-- that show some basic info and so on -->
+<body style="min-width: 450px;">
+	
 
 
-	<!-- LOGIN CARD -->
-	<main class="form-signin w-100 m-auto" id="loginCard">
-		<div class="card shadow p-4 bg-body" style="border-radius: 18px;">
-			<div class="card-body">
-				<form>
-					<div class="d-flex justify-content-center">
-						<h1 class="h3 mb-3 fw-normal"><b>Login</b></h1>
-					</div>
-					<div class="form-floating">
-						<input type="text" class="form-control" id="usernameInput" placeholder="Username">
-						<label for="usernameInput">Username</label>
-					</div>
-					<div class="form-floating">
-						<input type="password" class="form-control" id="passwordInput" placeholder="Password">
-						<label for="passwordInput">Password</label>
-					</div>		
-					<div class="d-flex justify-content-center">	
-						<button class="btn w-45 btn-dark myBtn mt-2" id="loginBtn" type="submit">LOGIN</button>
-					</div>
-					<div class="d-flex justify-content-center">	
-						<p class="mt-5 mb-3 text-body-secondary">&copy;Maksym Kintor 2023</p>
-					</div>				
-				</form>
-			</div>
-		</div>
-	</main> 
+
+	
 	
 	<!-- Bootstrap JS and Popper -->
 	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
-
-
-
 </body>
 </html>
--- login.php
+++ login.php
@@ -1,58 +1,77 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!-- Bootstrap -->
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-	<!-- jQuery -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
-	<!-- CSS -->
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<!-- Own JS -->
-	<script type="text/javascript" src="script.js"></script>
-	<!-- Google Icons -->
-	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-	<title>Log into system</title>
-	
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <!-- Own JS -->
+    <script type="text/javascript" src="script.js"></script>
+    <!-- Google Icons -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <title>Login</title>
 </head>
-<body>
-	<!-- NAVBAR -->
-	<!-- that show some basic info and so on -->
 
+<body style="min-width: 450px;">
+    <!-- NAVBAR -->
+    <!-- that show some basic info and so on -->
+    <!-- LOGIN CARD -->
+    <main class="form-signin w-100 m-auto" id="loginCard">
+        <div class="card shadow p-4 bg-body" style="border-radius: 18px;">
+            <div class="card-body">
+                <form>
+                    <div class="d-flex justify-content-center">
+                        <h1 class="h3 mb-3 fw-normal"><b>Login</b></h1>
+                    </div>
+                    <div class="form-floating">
+                        <input type="text" class="form-control" id="usernameInput" placeholder="Username" style=" margin-bottom: -1px;
+                        border-bottom-right-radius: 0;
+                        border-bottom-left-radius: 0;" required autocomplete="given-name">
+                        <label for="usernameInput">Username</label>
+                    </div>
+                    <div class="form-floating">
+                        <input type="password" class="form-control" id="passwordInput" placeholder="Password" style="margin-bottom: 10px;
+                        border-top-left-radius: 0;
+                        border-top-right-radius: 0;" required autocomplete="new-password">
+                        <label for="passwordInput">Password</label>
+                        <i class="material-icons eye" id="passwordEye">visibility_off</i>
+                    </div>
+                    <div class="d-flex justify-content-center">
+                        <button class="btn w-45 btn-dark myBtn mt-2" id="loginBtn" type="submit">LOGIN</button>
+                    </div>
+                    <div class="d-flex justify-content-center mt-3">
+                    	Don't Have an Account?
+                        <a href="registration.php">&nbsp;Register</a>
+                    </div>
+                    <div class="d-flex justify-content-center">
+                        <p class="mt-3 mb-3 text-body-secondary">&copy;Maksym Kintor 2023</p>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </main>
+    <!-- Bootstrap JS and Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+    <script type="text/javascript">
+        $(document).ready(function() {
 
-	<!-- LOGIN CARD -->
-	<main class="form-signin w-100 m-auto" id="loginCard">
-		<div class="card shadow p-4 bg-body" style="border-radius: 18px;">
-			<div class="card-body">
-				<form>
-					<div class="d-flex justify-content-center">
-						<h1 class="h3 mb-3 fw-normal"><b>Login</b></h1>
-					</div>
-					<div class="form-floating">
-						<input type="text" class="form-control" id="usernameInput" placeholder="Username">
-						<label for="usernameInput">Username</label>
-					</div>
-					<div class="form-floating">
-						<input type="password" class="form-control" id="passwordInput" placeholder="Password">
-						<label for="passwordInput">Password</label>
-					</div>		
-					<div class="d-flex justify-content-center">	
-						<button class="btn w-45 btn-dark myBtn mt-2" id="loginBtn" type="submit">LOGIN</button>
-					</div>
-					<div class="d-flex justify-content-center">	
-						<p class="mt-5 mb-3 text-body-secondary">&copy;Maksym Kintor 2023</p>
-					</div>				
-				</form>
-			</div>
-		</div>
-	</main> 
-	
-	<!-- Bootstrap JS and Popper -->
-	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+            $("#passwordEye").click(function() {
+                if ($("#passwordEye").html() == "visibility_off") {
+                    $("#passwordEye").html("visibility");
+                    $("#passwordInput").attr('type', 'text');
+                } else {
+                    $("#passwordEye").html("visibility_off");
+                    $("#passwordInput").attr('type', 'password');
+                }
 
+            });
 
-
+        });
+    </script>
 </body>
 </html>
