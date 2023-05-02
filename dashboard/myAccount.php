1d8d15d53f98ab355a804f963b2f1dec7e14601d
Maksym Kintor <kintormaksim@gmail.com>
2023-05-03 00:16:06 +0200

    dashboard pages

--- dashboard/myAccount.php
+++ dashboard/myAccount.php
@@ -1,25 +1,3 @@
-<?php
-session_start();
-require "config.php";
-
-//check if user is logged in, else throw him away
-if(!empty($_SESSION["id"])){
-	//logout condition
-	if((time() - $_SESSION['last_login_timestamp']) > 600){ //60 * 10 = 600 seconds
-		header('Location: logout.php');
-	}else{
-		$_SESSION['last_login_timestamp'] = time();
-		$id = $_SESSION["id"];
-		$user = $conn->prepare("select * from `User` where ID = :id");
-		$user->execute([':id' => $id]);
-	}
-	
-}
-else{
-	header("Location: login.php");
-}
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -37,7 +15,7 @@ else{
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<!-- Font Awesome -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-	<title>Home</title>
+	<title>My Account</title>
 	
 </head>
 <body style="min-width: 450px; " class="d-flex flex-column min-vh-100">
@@ -45,7 +23,7 @@ else{
 	<nav class="navbar navbar-expand-md bg-body-tertiar" style="margin:0px; padding:0px;">
 		<div class="container-fluid" style="background-color: rgb(192, 192, 192);">
 			<span class="navbar-brand mb-0 h1">
-				<a href="main.php"><img src="ezgif.com-gif-maker (1).png" style="width: 190px;"></a>
+				<a href="main.php"><img src="..\ezgif.com-gif-maker (1).png" style="width: 190px;"></a>
 			</span>
 			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
@@ -54,25 +32,19 @@ else{
 			<div class="collapse navbar-collapse" id="navigation">
 				<ul class="navbar-nav me-auto mb-2 mb-md-0">
 					<li class="nav-item d-flex justify-content-center">
-						<a class="nav-link active" aria-current="page" href="#" style="display: inline-block;">Dashboard</a>
+						<a class="nav-link" href="..\index.php" style="display: inline-block;">Dashboard</a>
 					</li>
 					<li class="nav-item d-flex justify-content-center">
-						<a class="nav-link" href="dashboard/myAccount.php" style="display: inline-block;">My Account</a>
+						<a class="nav-link active" aria-current="page" href="#" style="display: inline-block;">My Account</a>
 					</li>
 					<li class="nav-item d-flex justify-content-center">
-						<a class="nav-link" href="#" style="display: inline-block;">Contact Us</a>
+						<a class="nav-link" href="contactUs.php" style="display: inline-block;">Contact Us</a>
 					</li>
 				</ul>
 			</div>
 		</div>
 	</nav>
-
-	<div class="container flex-column" style="display: flex; align-items: center;">
-		<h1>Welcome <?php echo $user->fetchColumn(1); ?> it's index</h1>
-
-		<a href="logout.php">Logout</a>
-	</div>
-
+	my account
 	<!-- FOOTER -->
 	<div class="container mt-auto" style=" bottom: 0; ">
 		<footer class="d-flex flex-wrap justify-content-center justify-content-md-between align-items-center py-3 my-4 border-top">
