f30ae00e4b676cc8250064a866f96933bc7c6839
Maksym Kintor <kintormaksim@gmail.com>
2023-06-02 15:57:07 +0200

    final commit

--- index.php
+++ index.php
@@ -103,56 +103,5 @@ else{
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
 	<script src="C:\Users\kinto\node_modules\chart.js\dist\chart.umd.js"></script>
 	<script src="text/javascript"></script>
-	
-	<script type="text/javascript">
-		/*
-		window.onload = function () {
-
-			var dps = [];
-			var chart = new CanvasJS.Chart("chartContainer", {
-				exportEnabled: true,
-				title :{
-					text: ""
-				},
-				data: [{
-					type: "spline",
-					markerSize: 0,
-					dataPoints: dps 
-				}]
-			});
-
-			var xVal = 0;
-			var yVal = 100;
-			var updateInterval = 100;
-			var dataLength = 50; // number of dataPoints visible at any point
-
-			var updateChart = function (count) {
-				count = count || 1;
-				// count is number of times loop runs to generate random dataPoints.
-				for (var j = 0; j < count; j++) {	
-					
-
-					yVal = yVal + Math.round(5 + Math.random() *(-5-5));
-					
-					dps.push({
-						x: xVal,
-						y: yVal
-					});
-					xVal++;
-				}
-				if (dps.length > dataLength) {
-					dps.shift();
-				}	
-				chart.render();
-			};	
-
-			updateChart(dataLength); 
-			setInterval(function(){ updateChart() }, updateInterval); 
-
-		}
-		*/
-	</script>
-
-
 </body>
 </html>
